c8c9cf808e15ca5e26e4612a584931ac0b92256d
Mari Vaypane <avemari98@gmail.com>
2023-08-28 19:39:34 +0300

    add style to card

---
+++ src/components/CategoryCard/CategoryCard.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+    h2 {
+        margin-bottom: 0px;
+        margin-left: 26px;
+        margin-top: 0px;
+        font-weight: 500; 
+        font-size: 24px; 
+    }
+    
+    .card {
+        width: 340px;
+        height: 525px; 
+        border-radius: 34px;
+        border: 3px solid #622CC7;
+        background: #FAF9EE; 
+    }
+
+`
