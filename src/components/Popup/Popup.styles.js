5e1b15e885f85902cfad02eafa0c03c0037e4a40
Mari Vaypane <avemari98@gmail.com>
2023-08-29 02:15:08 +0300

    add basic style to popup

--- src/components/Popup/Popup.styles.js
+++ src/components/Popup/Popup.styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
 export const Block = styled.div`
+    position: absolute;
+    top: 15%;
+    width: 391px;
+    height: 392px;  
+    color: #fff;
+    border-radius: 26px;
+    //background: ${props => props.view === "create-contact" ? 'red' : 'blue'};
 
+    .close-btn {
+        position: absolute;
+        background: none;
+        border: none;
+        font-size: 30px;
+        color: #fff;
+        right: 4%;
+        top: 2%;
+      }
+    }
 
+   
 
 `
