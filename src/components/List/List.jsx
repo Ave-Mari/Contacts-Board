d9395ff4b5f44955338812d874cf1c773525afa2
Mari Vaypane <avemari98@gmail.com>
2023-08-28 19:28:20 +0300

    add card to list

---
+++ src/components/List/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//components
+import CategoryCard from './../CategoryCard/CategoryCard'
+//styles
+import { Wrapper } from './List.styles'
+
+
+export default function () {
+  return (
+    <Wrapper>
+      <CategoryCard category="Unsorted"/>
+      <CategoryCard category="Personal"/>
+      <CategoryCard category="Family"/>
+      <CategoryCard category="Work"/>        
+    </Wrapper>
+  )
+}
