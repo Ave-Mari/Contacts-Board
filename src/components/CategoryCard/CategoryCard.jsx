c39e4e5bf3eb6f322cfcbd6ace971336cda57b3f
Mari Vaypane <avemari98@gmail.com>
2023-08-28 19:39:19 +0300

    add elements to card

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -4,8 +4,11 @@ import { Wrapper } from './CategoryCard.styles'
 export default function CategoryCard({category}) {
   return (
     <Wrapper>
+        
+        <div className='category-card'>
         <h2>{category}</h2>
-        <div></div>
+        <div className='card'></div>
+        </div>
 
     </Wrapper>
   )
