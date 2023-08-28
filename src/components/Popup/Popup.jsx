864e24ac38c0a27dc460150dd029da0eca88ac55
Mari Vaypane <avemari98@gmail.com>
2023-08-29 02:15:29 +0300

    add button and content prop to popup component

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Block } from './Popup.styles'
 
-export default function Popup() {
+export default function Popup({ content }) {
   return (
     <Block>
-        
+        <button className='close-btn'>✕</button>
+
+        {content}
+
     </Block>
   )
 }
