87c043f2a3d6548d5c3df6296f571d76bcebd23e
Mari Vaypane <avemari98@gmail.com>
2023-08-29 02:14:44 +0300

    add popup with form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 //components
 import Header from './components/Header/Header';
 import List from './components/List/List';
+import Popup from './components/Popup/Popup';
 //styles
 import './index.css';
 
@@ -9,6 +10,53 @@ function App() {
 
   return (
     <main>
+      {/* <Popup 
+        content={
+          <div className='info-content'>
+          <p>This application can help you create and sort your contacts. 
+          After adding your contact to list you can move it through categories using drag and drop feature. You can also open your contact from list to see more info or delete it from list.
+          </p>
+          <p>Github: https://github.com/Ave-Mari </p>
+          </div>
+
+        }
+      /> */}
+      <Popup 
+        view='create-contact'
+        content={
+          <div className='create-content'>
+            <h3>Create contact</h3>
+
+            <input type="text" placeholder='Name'/>
+            <input type="text" placeholder='Phone'/>
+            <input type="email" placeholder='E-mail'/>
+
+            <p>Category:</p>
+            <div className="radio-block">
+              <label>
+                <input type="radio" />
+                  Uncorted
+              </label>
+              <label>
+                <input type="radio" />
+                  Personal
+              </label>
+              <label>
+                <input type="radio" />
+                  Family
+              </label>
+              <label>
+                <input type="radio" />
+                  Work
+              </label>
+              
+              
+            </div>
+
+          </div>
+        }
+      />
+      
       <Header />
       <List />
     </main>
