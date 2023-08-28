5ce761c29923eca6787c37a2a301908701dfc98c
Mari Vaypane <avemari98@gmail.com>
2023-08-29 00:16:22 +0300

    add slice to store

---
+++ src/store/ContactsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsSlice({
+    name: 'contacts',
+    initialState: {
+        contacts: []
+    },
+    reducers: 
+    {
+        addContact() {},
+        removeContact() {}
+    }
+})
