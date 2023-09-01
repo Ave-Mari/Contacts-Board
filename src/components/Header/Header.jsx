7d50206b485a4c60235232ac641915f18f4be1af
Mari Vaypane <avemari98@gmail.com>
2023-09-02 00:27:33 +0300

    add function to buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Wrapper } from './Header.styles'
 
-export default function Header({openPopup}) {
+export default function Header({openForm, openInfo}) {
   return (
     <Wrapper> 
         <h1>Contacts Board</h1>
-        <button className='info-btn'>i</button>
-        <button className='add-btn' onClick={openPopup}>+</button>
+        <button className='info-btn' onClick={openInfo}>i</button>
+        <button className='add-btn' onClick={openForm}>+</button>
     </Wrapper>
   )
 }
