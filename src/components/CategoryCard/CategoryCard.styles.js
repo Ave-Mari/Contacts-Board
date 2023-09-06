74b5d8075d39645a6ea6bf7ba1f9a48317adfcdb
Mari Vaypane <avemari98@gmail.com>
2023-09-06 14:21:37 +0300

    remove user select from card name

--- src/components/CategoryCard/CategoryCard.styles.js
+++ src/components/CategoryCard/CategoryCard.styles.js
@@ -7,6 +7,13 @@ export const Wrapper = styled.section`
         margin-top: 0px;
         font-weight: 500; 
         font-size: 24px; 
+
+        -webkit-touch-callout: none; 
+        -webkit-user-select: none; 
+         -khtml-user-select: none;
+           -moz-user-select: none; 
+            -ms-user-select: none; 
+                user-select: none;
     }
     
     .card {
