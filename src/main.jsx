8fc148d697a1c030e41d39de342c5e3156d882c9
Mari Vaypane <avemari98@gmail.com>
2023-09-23 15:45:51 +0300

    add store to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App.jsx';
+import { render } from 'react-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import App from './App.jsx'
+import rootReducer from './store/store.js'
 
-import store from './store/index.js';
+const store = configureStore({ reducer: rootReducer })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+render(
   <Provider store={store}>
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </Provider> 
-,
+  </Provider>,
+  document.getElementById('root')
 )
+ 
