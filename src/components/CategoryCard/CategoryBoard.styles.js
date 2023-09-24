62898b330f95ecb62390d82255e9c6d201173fae
Mari Vaypane <avemari98@gmail.com>
2023-09-24 22:13:42 +0300

    add flex to card

--- src/components/CategoryCard/CategoryBoard.styles.js
+++ src/components/CategoryCard/CategoryBoard.styles.js
@@ -1,27 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.section`
-    h2 {
-        margin-bottom: 0px;
-        margin-left: 26px;
-        margin-top: 0px;
-        font-weight: 500; 
-        font-size: 24px; 
+  .category-card {
+  }
 
-        -webkit-touch-callout: none; 
-        -webkit-user-select: none; 
-         -khtml-user-select: none;
-           -moz-user-select: none; 
-            -ms-user-select: none; 
-                user-select: none;
-    }
-    
-    .card {
-        width: 340px;
-        height: 525px; 
-        border-radius: 34px;
-        border: 3px solid #622CC7;
-        background: #FAF9EE; 
-    }
+  h2 {
+    margin-bottom: 0px;
+    margin-left: 26px;
+    margin-top: 0px;
+    font-weight: 500;
+    font-size: 24px;
 
-`
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .card {
+    width: 340px;
+    height: 525px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    overflow: hidden;
+    border-radius: 34px;
+    border: 3px solid #622cc7;
+    background: #faf9ee;
+  }
+`;
