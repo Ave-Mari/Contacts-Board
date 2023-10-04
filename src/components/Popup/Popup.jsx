60e76fd83d2eefb48fa7714e0107522385091ffe
Mari Vaypane <avemari98@gmail.com>
2023-10-04 15:16:55 +0300

    format document

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { PopupWrapper } from "./Popup.styles";
 
 export default function Popup({ content, view, closePopup }) {
+
+  
   return (
     <PopupWrapper className="wrapper" onClick={closePopup}>
       <div className="popup-block" view={view}>
